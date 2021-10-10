03f4a07247308ee648bced2510eaa9520b04f9f9
tenmasato <narito1015@icloud.com>
2021-10-10 11:09:26 +0000

    Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,13 +25,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-    redirect_to book_path(book.id)
-  else
-    book=Book.all
+    @book = Book.find(params[:id])
+   if @book.update(book_params)
+    redirect_to book_path(@book.id)
+    else
     render:edit
-  end
+    end
     flash[:notice] = "Book was successfully updated."
   end
 
